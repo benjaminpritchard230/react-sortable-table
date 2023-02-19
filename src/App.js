16bda29ed9b6ad61ba1758f984d591723b680d36
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-02-19 23:52:10 +0000

    Sortable table component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import Table from "./components/Table";
 import tableData1 from "./tableData1.json";
-import RenderAnotherTable from "./components/RenderAnotherTable";
 
 const columns = [
-  { label: "Full Name", accessor: "full_name", sortable: true },
-  { label: "Email", accessor: "email", sortable: false },
-  { label: "Gender", accessor: "gender", sortable: true, sortbyOrder: "desc" },
-  { label: "Age", accessor: "age", sortable: true },
-  { label: "Start date", accessor: "start_date", sortable: true },
+  { label: "Title", accessor: "title", sortable: true },
+  { label: "Release date", accessor: "release_date", sortable: true },
+  {
+    label: "Character",
+    accessor: "character",
+    sortable: true,
+    sortbyOrder: "desc",
+  },
 ];
 
 const App = () => {
   return (
     <div className="table_container">
-      <h1>Reusable sortable table with React</h1>
       <Table
         caption="Developers currently enrolled in this course. The table below is ordered (descending) by the Gender column."
-        data={tableData1}
+        data={tableData1.cast}
         columns={columns}
       />
-      <br />
-      <RenderAnotherTable />
-      <br />
     </div>
   );
 };
